8e59e8b34f5f9afc7e4935c19fbb862f1fb2604b
Colby Money <122302338+ColbyMoney@users.noreply.github.com>
2023-02-25 15:43:39 -0600

    Update Bowler.js
    connects frontend to the backend MySQL database and display database contents on frontend

--- bowlersconnected_frontend/src/components/Bowler.js
+++ bowlersconnected_frontend/src/components/Bowler.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {useState} from "react";
+import {useState, useEffect} from "react";
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import { Container,Paper,Button } from '@material-ui/core';
@@ -8,12 +8,28 @@ export default function Bowler() {
     const paperStyle={padding:"50px 20px", width:600,margin:"20px auto"}
     const[name,setName]=useState("")
     const[average,setAverage]=useState("")
+    const[bowlers,setBowlers]=useState([])
 
     const handleClick=(e)=>{
         e.preventDefault()
         const bowler={name,average}
         console.log(bowler)
+        fetch("http://localhost:8080/bowler/add",{
+            method:"POST",
+            headers:{"Content-Type":"application/json"},
+            body:JSON.stringify(bowler)
+        }).then(()=>{
+            console.log("New Bowler added")
+        })
     }
+
+    useEffect(()=>{
+        fetch("http://localhost:8080/bowler/getAll")
+        .then(res=>res.json())
+        .then((result)=>{
+            setBowlers(result);
+        })
+    },[])
 
   return (
     <Container>
@@ -40,6 +56,18 @@ export default function Bowler() {
                 Submit
             </Button>
             </Box>
+        </Paper>
+
+        <h1>Bowlers</h1>
+        <Paper elevation={3} style={paperStyle}>
+            {bowlers.map(bowler=>(
+                <Paper elevation={6} style={{margin:"10px",padding:"15px",textAlign:"left"}} key={bowler.id}>
+                    Id:{bowler.id}<br/>
+                    Name:{bowler.name}<br/>
+                    Average:{bowler.average}<br/>
+                </Paper>
+            ))}
+            
         </Paper>
     </Container>
   );
